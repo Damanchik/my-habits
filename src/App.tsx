ca91bd2c30863b62167ed27618c4a8e406beb44a
Pasechnik Vladislav <develop.pvv@gmail.com>
2020-05-07 11:11:29 +0300

    Добавляет минимальные asset'ы для приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Logo from './components/Logo';
 
-const App = () => <h1>Habits!</h1>;
+const App = () => (
+  <>
+    <CssBaseline />
+    <AppBar position="static">
+      <Container>
+        <Logo />
+      </Container>
+    </AppBar>
+    <Container component="main">
+      <Typography variant="h3">Fill free and make changes!</Typography>
+    </Container>
+  </>
+);
 
 export default App;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Logo = () => <Typography variant="h5">Habits</Typography>;
+
+export default Logo;
