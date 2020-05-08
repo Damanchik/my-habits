7ec2158660f2715603c9361463d747b9b9b0907b
mfs9408 <59794609+mfs9408@users.noreply.github.com>
2020-05-08 11:57:01 +0300

    Добавляет роутинг в проект.

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,29 +3,49 @@ import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import Logo from '../Logo';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from 'react-router-dom';
 
 const AppBar = () => (
-  <MuiAppBar position="static" color="primary">
-    <Toolbar>
-      <Grid container justify="space-between" alignItems="center">
-        <Grid item>
-          <Logo />
-        </Grid>
-        <Grid item>
-          <Box pr={1} component="span">
-            <Link href="#foo" color="inherit">
-              Войти
+  <Router>
+    <MuiAppBar position="static" color="primary">
+      <Toolbar>
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item>
+            <Logo />
+          </Grid>
+          <Grid item>
+            <Box pr={1} component="span">
+              <Link to="/login" color="inherit">
+                Войти
+              </Link>
+            </Box>
+            <Link to="/register" color="inherit">
+              Регистрация
             </Link>
-          </Box>
-          <Link href="#foo" color="inherit">
-            Регистрация
-          </Link>
+          </Grid>
         </Grid>
-      </Grid>
-    </Toolbar>
-  </MuiAppBar>
+      </Toolbar>
+      <Switch>
+        <Route path="/login">
+          <div> Дратути! </div>
+        </Route>
+        <Route path="/register"></Route>
+        <Route path="/logout"></Route>
+        <Route path="/tasks"></Route>
+        <Route path="/goals"></Route>
+        <Route path="/goals/add"></Route>
+        <Route path="/"></Route>
+      </Switch>
+    </MuiAppBar>
+  </Router>
 );
 
 export default AppBar;
