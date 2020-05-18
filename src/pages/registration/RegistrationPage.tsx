8052e3e225f45d095092dadf3374a4459de7fab8
mfs9408 <59794609+mfs9408@users.noreply.github.com>
2020-05-18 17:24:11 +0300

    Добавляет размытый фон, создаёт боксы для корректной работы ошибок при регистрации на странице регистрации.

--- src/pages/registration/RegistrationPage.tsx
+++ src/pages/registration/RegistrationPage.tsx
@@ -4,6 +4,8 @@ import { useForm } from 'react-hook-form';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import './RegistrationPage.styles.css';
+import Box from '@material-ui/core/Box';
 
 const RegistrationPage = () => {
   const methods = useForm();
@@ -17,72 +19,118 @@ const RegistrationPage = () => {
       container
       justify="center"
       alignItems="center"
-      style={{ background: 'none', height: 'calc(100% - 64px)' }}
+      className="registrationBackground"
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
         style={{
           background: 'none',
           width: 'auto',
-          height: '55vh',
+          height: '60vh',
           minHeight: '550px',
         }}
       >
         <Grid
           container
           direction="column"
-          justify="space-around"
+          justify="space-between"
           alignItems="center"
           style={{ height: '100%', width: '100%' }}
         >
-          <Typography align="center" variant="h4">
+          <Typography align="center" variant="h4" color="primary">
             Регистрация
           </Typography>
-          <TextField
-            name="firstName"
-            variant="outlined"
-            label="Ваше имя"
-            inputRef={register({
-              required: 'Введите ваше имя',
-              minLength: {
-                value: 3,
-                message: 'Минимум 3 символа',
-              },
-            })}
-          />
-          {errors.firstName && <p>{errors.firstName.message}</p>}
-          <TextField
-            name="secondName"
-            variant="outlined"
-            label="Ваша фамилия"
-            inputRef={register}
-          />
-          <TextField
-            name="password"
-            variant="outlined"
-            label="Придумайте пароль"
-            type="password"
-            inputRef={register({
-              required: 'Вам нужно придумать пароль',
-              minLength: {
-                value: 5,
-                message: 'Пароль должен включать 5 символов',
-              },
-            })}
-          />
-          {errors.password && <p>{errors.password.message}</p>}
-          <TextField
-            name="passwordRepeat"
-            variant="outlined"
-            type="password"
-            label="Повторите пароль"
-            inputRef={register({
-              validate: value =>
-                value === password.current || 'Пароль не совпадает',
-            })}
-          />
-          {errors.passwordRepeat && <p>{errors.passwordRepeat.message}</p>}
-          <Button type="submit" variant="outlined">
+          <Box
+            component="div"
+            style={{
+              background: 'none',
+              width: '190px',
+              height: '70px',
+            }}
+          >
+            <TextField
+              color="primary"
+              name="firstName"
+              variant="outlined"
+              label="Ваше имя"
+              inputRef={register({
+                required: 'Введите ваше имя',
+                minLength: {
+                  value: 3,
+                  message: 'Минимум 3 символа',
+                },
+              })}
+            />
+            {errors.firstName && (
+              <span style={{ color: 'red' }}>{errors.firstName.message}</span>
+            )}
+          </Box>
+          <Box
+            component="div"
+            style={{
+              background: 'none',
+              width: '190px',
+              height: '70px',
+            }}
+          >
+            <TextField
+              name="secondName"
+              variant="outlined"
+              label="Ваша фамилия"
+              inputRef={register}
+            />
+          </Box>
+          <Box
+            component="div"
+            style={{
+              background: 'none',
+              width: '190px',
+              height: '70px',
+            }}
+          >
+            <TextField
+              name="password"
+              variant="outlined"
+              label="Придумайте пароль"
+              type="password"
+              inputRef={register({
+                required: 'Вам нужно придумать пароль',
+                minLength: {
+                  value: 5,
+                  message: 'Пароль должен включать 5 символов',
+                },
+              })}
+            />
+            {errors.password && (
+              <span style={{ color: 'red' }}>{errors.password.message}</span>
+            )}
+          </Box>
+          <Box
+            component="div"
+            style={{
+              background: 'none',
+              width: '190px',
+              height: '70px',
+            }}
+          >
+            <TextField
+              name="passwordRepeat"
+              variant="outlined"
+              type="password"
+              label="Повторите пароль"
+              color="primary"
+              inputRef={register({
+                validate: value =>
+                  value === password.current || 'Пароль не совпадает',
+              })}
+            />
+            {errors.passwordRepeat && (
+              <span style={{ color: 'red' }}>
+                {errors.passwordRepeat.message}
+              </span>
+            )}
+          </Box>
+          <Button type="submit" variant="outlined" color="primary">
             Создать аккаунт
           </Button>
         </Grid>
