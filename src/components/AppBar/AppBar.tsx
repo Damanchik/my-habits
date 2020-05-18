2373508fb2495aa2a5302be43e33b275856796c7
mfs9408 <59794609+mfs9408@users.noreply.github.com>
2020-05-18 13:35:31 +0300

    Убирает ts-ignore и тег ThemeProvider

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -3,34 +3,25 @@ import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import { ThemeProvider } from '@material-ui/core/styles';
 import Logo from '../Logo';
 import Link from '../Link';
-import theme from '../theme';
 
-// @ts-ignore
 const AppBar = () => (
-  <ThemeProvider theme={theme}>
-    <MuiAppBar position="static" color="primary">
-      <Toolbar>
-        <Grid container justify="space-between" alignItems="center">
-          <Grid item>
-            <Logo />
-          </Grid>
-          <Grid item>
-            <Box pr={1} component="span">
-              <Link label="Войти" to="/login" color="inherit" />
-            </Box>
-            <Link
-              label="Зарегистрироваться"
-              to="/registration"
-              color="inherit"
-            />
-          </Grid>
+  <MuiAppBar position="static" color="primary">
+    <Toolbar>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Logo />
         </Grid>
-      </Toolbar>
-    </MuiAppBar>
-  </ThemeProvider>
+        <Grid item>
+          <Box pr={1} component="span">
+            <Link label="Войти" to="/login" color="inherit" />
+          </Box>
+          <Link label="Зарегистрироваться" to="/registration" color="inherit" />
+        </Grid>
+      </Grid>
+    </Toolbar>
+  </MuiAppBar>
 );
 
 export default AppBar;
