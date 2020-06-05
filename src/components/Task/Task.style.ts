5382e2c7d27ade74c1bb12bf47b6b65db6747e12
mfs9408 <482Muratidi300>
2020-06-05 12:38:22 +0300

    Добавляет Task.style.ts

---
+++ src/components/Task/Task.style.ts
@@ -0,0 +1,32 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+export default makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      padding: theme.spacing(2),
+    },
+    paperContainer: {
+      margin: theme.spacing(2),
+    },
+    moodBadIcon: {
+      color: 'orange',
+    },
+    partiallyDone: {
+      marginRight: 17,
+    },
+    doneOutlineIcon: {
+      marginLeft: 44,
+      color: 'green',
+    },
+    img: {
+      height: '150px',
+      width: '100%',
+      objectFit: 'cover',
+    },
+    typographyDescription: {
+      [theme.breakpoints.up('sm')]: {
+        textAlign: 'left',
+      },
+    },
+  })
+);
