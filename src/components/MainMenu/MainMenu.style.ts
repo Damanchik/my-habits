ccaa01208c488b0c9af4525f092fb7f62b6709e7
mfs9408 <482Muratidi300>
2020-06-05 15:14:50 +0300

    Добавляет стили к MainMenu.tsx

---
+++ src/components/MainMenu/MainMenu.style.ts
@@ -0,0 +1,29 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+export default makeStyles((theme: Theme) =>
+  createStyles({
+    avatar: {
+      width: theme.spacing(8),
+      height: theme.spacing(8),
+    },
+    typography: {
+      background: 'none',
+      marginTop: theme.spacing(2),
+    },
+    gridSideBar: {
+      height: '100%',
+    },
+    gridLogo: {
+      flexGrow: 1,
+    },
+    list: {
+      flexGrow: 6,
+    },
+    gridAvatar: {
+      flexGrow: 1,
+    },
+    input: {
+      display: 'none',
+    },
+  })
+);
--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-/*
 import ListItem from '@material-ui/core/ListItem';
 import List from '@material-ui/core/List';
 import InboxIcon from '@material-ui/core/Icon';
@@ -14,70 +13,69 @@ import Typography from '@material-ui/core/Typography';
 import AssignmentTurnedInIcon from '@material-ui/core/Icon';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-
- */
+import UseStyles from './MainMenu.style';
 
 const MainMenu = () => {
+  const classes = UseStyles();
   return (
-    <div>asjg</div>
-    /*
-    <List className={classes.list}>
-      <ListItem button>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText>
-          <Typography>Задания</Typography>
-        </ListItemText>
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <AssignmentTurnedInIcon />
-        </ListItemIcon>
-        <ListItemText>
-          <Typography>Успехи</Typography>
-        </ListItemText>
-      </ListItem>
-    </List>
-  <Divider />
-  <Grid
-    container
-    direction="column"
-    justify="center"
-    alignItems="center"
-    wrap="wrap"
-    className={classes.gridAvatar}
-  >
-    <Menu
-      id="simple-menu"
-      anchorEl={anchorEl}
-      keepMounted
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-    >
-      <Input
-        name="avatar-upload"
-        className={classes.input}
-        id="avatar-upload"
-        type="file"
-      />
-      <label htmlFor="avatar-upload">
-        <MenuItem onClick={handleClose}>Сменить аватар</MenuItem>
-      </label>
-      <MenuItem onClick={handleClose}>Выйти из аккаунта</MenuItem>
-    </Menu>
-    <IconButton onClick={handleClick}>
-      <Avatar className={classes.avatar} />
-    </IconButton>
-    <Typography
-      variant="body1"
-      align="center"
-      className={classes.typography}
-    >
-      Фёдор
-    </Typography>
-
-     */
+    <>
+      <List className={classes.list}>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText>
+            <Typography>Задания</Typography>
+          </ListItemText>
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <AssignmentTurnedInIcon />
+          </ListItemIcon>
+          <ListItemText>
+            <Typography>Успехи</Typography>
+          </ListItemText>
+        </ListItem>
+      </List>
+      <Divider />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        wrap="wrap"
+        className={classes.gridAvatar}
+      >
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Input
+            name="avatar-upload"
+            className={classes.input}
+            id="avatar-upload"
+            type="file"
+          />
+          <label htmlFor="avatar-upload">
+            <MenuItem onClick={handleClose}>Сменить аватар</MenuItem>
+          </label>
+          <MenuItem onClick={handleClose}>Выйти из аккаунта</MenuItem>
+        </Menu>
+        <IconButton onClick={handleClick}>
+          <Avatar className={classes.avatar} />
+        </IconButton>
+        <Typography
+          variant="body1"
+          align="center"
+          className={classes.typography}
+        >
+          Фёдор
+        </Typography>
+      </Grid>
+    </>
   );
 };
 
