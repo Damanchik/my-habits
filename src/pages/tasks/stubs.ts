653a17ce937983fa8f2d8f5f5a217c21efbae725
mfs9408 <482Muratidi300>
2020-06-05 16:58:54 +0300

    Добавляет задачи из заглушки stubs.ts на экран задач.

--- src/pages/tasks/stubs.ts
+++ src/pages/tasks/stubs.ts
@@ -2,25 +2,49 @@ const data = [
   {
     id: 1,
     name: 'Отжаться 10 раз',
-    description: 'Отжаться .......',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+      'malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum ' +
+      'dolor sit amet, consectetur adipiscing elit Suspendisse malesuada ' +
+      'lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada lacus ex, ' +
+      'sit ametmalesuada lacus exфы consectetur adipiscing elit Suspendisse ' +
+      'malesuada lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada',
     imgUrl: 'http://placekitten.com/600/900',
   },
   {
     id: 2,
     name: 'Поприседать 10 раз',
-    description: 'Отжаться .......',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+      'malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum ' +
+      'dolor sit amet, consectetur adipiscing elit Suspendisse malesuada ' +
+      'lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada lacus ex, ' +
+      'sit ametmalesuada lacus exфы consectetur adipiscing elit Suspendisse ' +
+      'malesuada lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada',
     imgUrl: 'http://placekitten.com/1000/10',
   },
   {
     id: 3,
     name: 'Бегать 100 км в день',
-    description: 'Отжаться .......',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     imgUrl: 'http://placekitten.com/10/1000',
   },
   {
     id: 4,
     name: 'Кушать овсянку по утрам',
-    description: 'Отжаться .......',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+      'malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum ' +
+      'dolor sit amet, consectetur adipiscing elit Suspendisse malesuada ' +
+      'lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada lacus ex, ' +
+      'sit ametmalesuada lacus exфы consectetur adipiscing elit Suspendisse ' +
+      'malesuada lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada' +
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ' +
+      'malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum ' +
+      'dolor sit amet, consectetur adipiscing elit Suspendisse malesuada ' +
+      'lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada lacus ex, ' +
+      'sit ametmalesuada lacus exфы consectetur adipiscing elit Suspendisse ' +
+      'malesuada lacus ex, sit amet.malesuada lacus ex, sit amet + malesuada',
     imgUrl: 'http://placekitten.com/10/1000',
   },
 ];
